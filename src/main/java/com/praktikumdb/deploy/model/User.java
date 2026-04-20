6b658fd4c437fe87abad56e9cce8ede3a7259134
abrayudhistira <abrayudhistira59@gmail.com>
2026-04-20 10:00:56 +0700

    properties of user model

--- src/main/java/com/praktikumdb/deploy/model/User.java
+++ src/main/java/com/praktikumdb/deploy/model/User.java
@@ -1,6 +1,8 @@
 package com.praktikumdb.deploy.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,4 +17,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class User {
 
+    @Id
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String nim;
 }
