e9d6aabd5a3e61bf6a5f97cd205b5a30e77ed783
abrayudhistira <abrayudhistira59@gmail.com>
2026-04-20 10:09:09 +0700

    add user service

--- src/main/java/com/praktikumdb/deploy/service/UserService.java
+++ src/main/java/com/praktikumdb/deploy/service/UserService.java
@@ -1,5 +1,8 @@
 package com.praktikumdb.deploy.service;
 
+import java.util.UUID;
+
+import com.praktikumdb.deploy.model.User;
 import com.praktikumdb.deploy.repository.UserRepository;
 
 public class UserService {
@@ -8,6 +11,11 @@ public class UserService {
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public User addUSer(User request) {
+        request.setId(UUID.randomUUID().toString());
+        return userRepository.save(request);
     }
 
 }
